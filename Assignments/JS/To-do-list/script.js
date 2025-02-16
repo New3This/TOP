fed6e5c19f1e515ca983c3050840dea9e6a110a9
Dylan <dylansinghabahu360@gmail.com>
2025-02-17 10:32:49 +1100

    todo item edit and delete saves to local storage

--- Assignments/JS/To-do-list/script.js
+++ Assignments/JS/To-do-list/script.js
@@ -4,73 +4,78 @@ const itemArea = document.getElementById("second-area");
 submitBtn.addEventListener("click", addItem);
 let localStorageItems;
 
-// function saveList(todoText) {
-//     localStorageItems.push(todoText.textContent);
-//     localStorage.setItem("todo-list", JSON.stringify(localStorageItems));
-//     console.log(JSON.parse(localStorage.getItem("todo-list")));
+function saveList(todoText) {
+    localStorageItems.push(todoText.textContent);
+    localStorage.setItem("todo-list", JSON.stringify(localStorageItems));
+    // console.log(JSON.parse(localStorage.getItem("todo-list")));
+}
+
+// function editList() {
+
 // }
 
-// function loadList() {
-//     localStorageItems = JSON.parse(localStorage.getItem("todo-list") || "[]");
-//     if (localStorageItems !== null) {
-//         localStorageItems.forEach(element => {
-//             let toDoItem = document.createElement("div");
-//             toDoItem.classList.add("todo-list-item");
-//             toDoItem.innerHTML = `
-//             <input type="checkbox" class="checkbox">
-//             <span class="todo-text" contenteditable="false"></span>
-//             <div class="todo-item-group">
-//                 <img src="imgs/edit.svg" alt="edit" class="editImg">
-//                 <img src="imgs/cross.svg" alt="edit" class="crossImg">
-//                 <img src="imgs/tick.svg" alt="edit" class="tickImg">
-//             </div>
-//             `;
-//             let todoText = toDoItem.querySelector(".todo-text");
-//             console.log(element);
-//             todoText.textContent = element;
+
+
+function loadList() {
+    localStorageItems = JSON.parse(localStorage.getItem("todo-list") || "[]");
+    if (localStorageItems !== null) {
+        localStorageItems.forEach(element => {
+            let toDoItem = document.createElement("div");
+            toDoItem.classList.add("todo-list-item");
+            toDoItem.innerHTML = `
+            <input type="checkbox" class="checkbox">
+            <span class="todo-text" contenteditable="false"></span>
+            <div class="todo-item-group">
+                <img src="imgs/edit.svg" alt="edit" class="editImg">
+                <img src="imgs/cross.svg" alt="edit" class="crossImg">
+                <img src="imgs/tick.svg" alt="edit" class="tickImg">
+            </div>
+            `;
+            let todoText = toDoItem.querySelector(".todo-text");
+            console.log(element);
+            todoText.textContent = element;
 
     
-//             let checkBox = toDoItem.querySelector(".checkbox");
-//             let todoItemGroup = toDoItem.querySelector(".todo-item-group");
-//             let editBtn = todoItemGroup.querySelector(".editImg");
-//             let crossBtn = todoItemGroup.querySelector(".crossImg");
-//             let tickBtn = todoItemGroup.querySelector(".tickImg");
+            let checkBox = toDoItem.querySelector(".checkbox");
+            let todoItemGroup = toDoItem.querySelector(".todo-item-group");
+            let editBtn = todoItemGroup.querySelector(".editImg");
+            let crossBtn = todoItemGroup.querySelector(".crossImg");
+            let tickBtn = todoItemGroup.querySelector(".tickImg");
 
-//             crossBtn.style.display = "none";
-//             tickBtn.style.display = "none";
+            crossBtn.style.display = "none";
+            tickBtn.style.display = "none";
 
-//             if (toDoItem.textContent.trim() !== "") {
-
-
-//                 function handleDel() {
-//                     deleteToDoText(toDoItem);
-//                 }
+            if (toDoItem.textContent.trim() !== "") {
 
 
-//                 itemArea.appendChild(toDoItem);
+                function handleDel() {
+                    deleteToDoText(toDoItem);
+                }
 
-//                 todoText.addEventListener("click", handleDel);
+
+                itemArea.appendChild(toDoItem);
+
+                todoText.addEventListener("click", handleDel);
                 
-//                 editBtn.addEventListener("click", () => editActive(todoText, toDoItem, crossBtn, tickBtn, editBtn));
+                editBtn.addEventListener("click", () => editActive(todoText, toDoItem, crossBtn, tickBtn, editBtn));
 
-//                 checkBox.addEventListener("change", () => {
-//                     if (checkBox.checked) {
-//                         todoText.style.textDecoration = "line-through";
-//                         todoText.style.color = "#808080";
-//                     }
-//                     else {
-//                         todoText.style.color = "#000000";
-//                         todoText.style.textDecoration = "none";
-//                     }
-//                 })
-//             }
-//         });
+                checkBox.addEventListener("change", () => {
+                    if (checkBox.checked) {
+                        todoText.style.textDecoration = "line-through";
+                        todoText.style.color = "#808080";
+                    }
+                    else {
+                        todoText.style.color = "#000000";
+                        todoText.style.textDecoration = "none";
+                    }
+                })
+            }
+        });
 
-//     }
+    }
+}
 
-// }
-
-// loadList();
+loadList();
 
 function addItem() {
 
@@ -130,9 +135,9 @@ function addItem() {
                 todoText.style.textDecoration = "none";
             }
         })
-        // saveList(todoText);
-        // itemArea.innerHTML = "";
-        // loadList();
+        saveList(todoText);
+        itemArea.innerHTML = "";
+        loadList();
     }
 
 
@@ -161,9 +166,17 @@ function editActive(todoText, toDoItem, crossBtn, tickBtn, editBtn) {
     tickBtn.style.display = "inline";
     editBtn.style.display = "none";
 
+
     tickBtn.addEventListener("click", () => {
+        const todoIndex = Array.from(itemArea.children).indexOf(toDoItem);
+        if (todoIndex !== -1) {
+            localStorageItems[todoIndex] = todoText.textContent;
+            localStorage.setItem("todo-list", JSON.stringify(localStorageItems));
+        }
+            
         tickCross(tickBtn, todoText, crossBtn, editBtn);
         todoText.addEventListener("click", handleDel);
+    
     })
 
     crossBtn.addEventListener("click", () => {
@@ -183,5 +196,11 @@ function tickCross(tickBtn, todoText, crossBtn, editBtn) {
 }
 
 function deleteToDoText(toDoItem) {
+    const todoIndex = Array.from(itemArea.children).indexOf(toDoItem);
+    if (todoIndex !== -1) {
+        localStorageItems.splice(todoIndex, 1);
+        localStorage.setItem("todo-list", JSON.stringify(localStorageItems));
+
+    }
     toDoItem.remove();
 }
